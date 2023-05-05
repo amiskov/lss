988c3ba7b6854f4f5a39d75a74d8dd6a30a0a889
Andrey Miskov <amiskov@gmail.com>
2023-05-05 08:30:59 +0300

    deploy draft

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ ALLOWED_HOSTS = [
 
 INTERNAL_IPS = (
     '127.0.0.1',
-    # '0.0.0.0',
+    '0.0.0.0',
     '192.168.1.96',
 )
 
@@ -162,10 +162,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+# STATIC_ROOT = BASE_DIR / "staticfiles" # TODO: use with Nginx
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
