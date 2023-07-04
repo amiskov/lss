3def099dcf016ee67d825f354833e1fb6f6be974
Andrey Miskov <amiskov@gmail.com>
2023-07-04 19:08:59 +0300

    refactor

--- activities/views/acted.py
+++ activities/views/acted.py
@@ -1,10 +1,12 @@
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponseServerError
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic import UpdateView
-from django.utils import timezone, dateparse
+from django.utils import dateparse
 from django.utils.timezone import get_current_timezone
 
-from ..models import ActedActivity, Activity
-from ..forms import ActivityForm
+from ..models import ActedActivity
+
 
 class ActedUpdateView(UpdateView):
     model = ActedActivity
@@ -25,11 +27,12 @@ def add_acted_activity(request, activity_id):
 
 def remove_acted_activity(request, actedactivity_id):
     acted_activity = ActedActivity.objects.get(id=actedactivity_id)
-    activity_name = acted_activity.activity.name
+    _activity_name = acted_activity.activity.name
     acted_activity.delete()
     # messages.success(
     #     request, f"<b>{activity_name}</b> was removed successfully!")
     return redirect('index')
+
 
 def show_time(request, pk):
     acted = ActedActivity.objects.get(pk=pk)
@@ -37,28 +40,36 @@ def show_time(request, pk):
         request, 'activities/_time.html', context={'acted': acted}
     )
 
+
 def change_time(request, pk):
     acted = ActedActivity.objects.get(pk=pk)
     if request.method == 'POST':
         tz = get_current_timezone()
 
-        # TODO:
-        # - change time in separate function
-        # - show form errors
-        # - check time: started must be earlier
-
-        current_started_localtime = acted.started.astimezone(tz)
+        # TODO: update time in separate function
         new_started_time = dateparse.parse_time(request.POST['started'])
-        new_started = current_started_localtime.replace(
-            hour=new_started_time.hour, minute=new_started_time.minute)
-        acted.started = new_started
-
-        current_finished_localtime = acted.finished.astimezone(tz)
         new_finished_time = dateparse.parse_time(request.POST['finished'])
-        new_finished = current_finished_localtime.replace(
-            hour=new_finished_time.hour, minute=new_finished_time.minute)
-        acted.finished = new_finished
 
+        if not new_started_time or not new_finished_time:
+            return HttpResponseServerError()
+
+        current_started_time = acted.started.astimezone(tz)
+        current_finished_time = acted.finished.astimezone(tz)
+
+        # start must be less than finish
+        if current_started_time > current_finished_time:
+            # TODO: show a message, not an error
+            return HttpResponseBadRequest()
+
+        new_started = current_started_time.replace(
+            hour=new_started_time.hour,
+            minute=new_started_time.minute
+        )
+        new_finished = current_finished_time.replace(
+            hour=new_finished_time.hour,
+            minute=new_finished_time.minute
+        )
+        acted.started, acted.finished = new_started, new_finished
         acted.save(update_fields=["started", "finished"])
 
         if request.htmx:
